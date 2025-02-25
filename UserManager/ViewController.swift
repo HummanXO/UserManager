ce09be2fa497b55ab43973a847d22ed8ea783500
HummanXO <rxkolchin@gmail.com>
2025-02-25 16:19:44 +0700

    Initial Commit

---
+++ UserManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserManager
+//
+//  Created by Alexandr on 2/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
